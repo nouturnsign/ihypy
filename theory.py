389ebef940bf4a6798270abab29ee979ca735961
nouturnsign <ericwangyy@gmail.com>
2022-05-01 12:12:26 -0400

    Introduced Interval and fixed Chord

--- theory.py
+++ theory.py
@@ -97,6 +97,65 @@ class NaturalMinorScale(AeolianScale):
     def __init__(self, octaves : int = 1):
         super().__init__(octaves)
 
+# TODO: write a Unit abstract class to establish equivalent between certain units
+# Unit might belong under systems
+
+class Interval(abc.ABC):
+    """An abstract class for intervals.
+    
+    Attributes
+    ----------
+    relation : int | float
+        How the two notes are related. This can be expressed in absolute (e.g. semitones) or relative (e.g. frequency ratio) units.
+    unit : str
+        The unit for the relation.
+    """
+
+    @abc.abstractmethod
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return "Interval{" + str(self.relation) + " " + self.unit + "}"
+
+    @property
+    def relation(self):
+        return self._relation
+
+    @property
+    def unit(self):
+        return self._unit
+
+class SemitoneInterval(Interval):
+    """An interval, in terms of absolute semitones."""
+
+    def __init__(self, semitones):
+        self._relation = semitones
+        self._unit = "semitones"
+
+    def __add__(self, other):
+        # e.g. SemitoneInterval(2) + SemitoneInterval(3) = SemitoneInterval(5)
+        # TODO: assert a valid sum
+        return SemitoneInterval(self.relation + other.relation)
+
+    def __mul__(self, semitones):
+        # e.g. SemitoneInterval(2) * 3 = SemitoneInterval(6)
+        # TODO: assert a valid product
+        return SemitoneInterval(self.relation * semitones)
+
+    def __rmul__(self, semitones):
+        return self.__mul__(semitones)
+
+class Semitone(SemitoneInterval):
+    """A Western semitone."""
+
+    def __init__(self):
+        super().__init__(1)
+
+# TODO: define the intervals under https://en.wikipedia.org/wiki/Interval_(music)#Main_intervals
+# TODO: have a music system actually be able to create a chord
+# TODO: have an instrument be able to play a chord
+
 class Chord(abc.ABC):
     """An abstract class describing a chord.
     
@@ -106,9 +165,10 @@ class Chord(abc.ABC):
         The list of frequency ratios from the tonic.
     """
 
+    @abc.abstractmethod
     def __init__(self):
         pass
 
     @property
-    def frequency_ratio(self):
-        return self._frequency_ratio
+    def intervals(self):
+        return self._intervals
