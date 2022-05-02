6508b698bf95c6b2506dd1de9b0e49f19d183e00
nouturnsign <ericwangyy@gmail.com>
2022-05-01 21:51:06 -0400

    Renamed files for simplicity

--- demonstration.py
+++
@@ -1,7 +0,0 @@
-from ihypy import *
-
-WCS = systems.WesternClassicalSystem()
-c_minor_scale_1_octave = WCS.create_scale(theory.NaturalMinorScale(octaves = 1), "C3")
-piano = instruments.Piano()
-
-piano.play_scale(c_minor_scale_1_octave, 6000)
